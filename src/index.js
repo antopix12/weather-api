753f9a69256bca9805283e334f42c8d063651f66
antopix <antopix33@gmail.com>
2025-04-11 12:27:07 -0700

    hardcoding API key is more convenient for now

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import "./styles.css";
-import { greeting } from "./greeting.js";
-
-// Access the API key that was defined in Webpack config via DefinePlugin
-console.log("Weather API Key:", process.env.VISUAL_CROSSING_API_KEY);
 
 const getWeather = async (city) => {
-  const apiKey = process.env.VISUAL_CROSSING_API_KEY;
-  const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=LAHQZ93VXZQDHXHHAZEJ8E6KU`);
+  const apiKey = `LAHQZ93VXZQDHXHHAZEJ8E6KU`;
+  const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${apiKey}`);
   const data = await response.json();
   console.log(data);
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,6 @@
 
-// const path = require("path");
-// const HtmlWebpackPlugin = require("html-webpack-plugin");
-
-// module.exports = {
-//   mode: "development",
-//   entry: "./src/index.js",
-//   output: {
-//     filename: "main.js",
-//     path: path.resolve(__dirname, "dist"),
-//     clean: true,
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: "./src/template.html",
-//     }),
-//   ],
-//   module: {
-//     rules: [
-//       {
-//         test: /\.css$/i,
-//         use: ["style-loader", "css-loader"],
-//       },
-//       {
-//         test: /\.html$/i,
-//         loader: "html-loader",
-//       },
-//       {
-//         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-//         type: "asset/resource",
-//       }
-//     ],
-//   },
-// };
-
-
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
 
 module.exports = {
   mode: "development",
@@ -49,9 +13,6 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
-    }),
-    new webpack.DefinePlugin({
-      'process.env.VISUAL_CROSSING_API_KEY': JSON.stringify(process.env.VISUAL_CROSSING_API_KEY || 'default_api_key'),
     }),
   ],
   module: {
